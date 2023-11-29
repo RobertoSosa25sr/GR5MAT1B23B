05ffb067cff8d2fabfc842f525883a6b358988f7
Roberto Sosa <93219782+RobertoSosa25sr@users.noreply.github.com>
2023-11-29 12:46:12 -0500

    Add files via upload

--- src/main/java/modelo/CatalogoLibros.java
+++ src/main/java/modelo/CatalogoLibros.java
@@ -1,5 +1,31 @@
 package modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CatalogoLibros {
 
+    private List<Libro> listaLibros;
+
+    public CatalogoLibros() {
+
+        listaLibros = new ArrayList<>();
+        listaLibros.add(new Libro("El Señor de los Anillos", "J.R.R. Tolkien", "Fantasía", "978-84-450-7425-0", true));
+        listaLibros.add(new Libro("Cien años de soledad", "Gabriel García Márquez", "Realismo mágico", "978-84-376-0494-7", true));
+        listaLibros.add(new Libro("1984", "George Orwell", "Ciencia ficción", "978-84-376-0494-7", false));
+
+    }
+
+    public static void main(String[] args) {
+
+        CatalogoLibros catalogo = new CatalogoLibros();
+        List<Libro> libros = catalogo.getListaLibros();
+        for (Libro libro : libros) {
+            System.out.println(libro);
+        }
+    }
+
+    public List<Libro> getListaLibros() {
+        return listaLibros;
+    }
 }
--- src/main/java/modelo/Libro.java
+++ src/main/java/modelo/Libro.java
@@ -2,4 +2,26 @@ package modelo;
 
 public class Libro {
 
+    private String titulo;
+    private String autor;
+    private String genero;
+    private String isbn;
+    private boolean disponibilidad;
+
+    public Libro(String titulo, String autor, String genero, String isbn, boolean disponibilidad) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.genero = genero;
+        this.isbn = isbn;
+        this.disponibilidad = disponibilidad;
+    }
+
+    @Override
+    public String toString() {
+        return "Título: " + titulo + "\nAutor: " + autor + "\nGénero: " + genero + "\nISBN: " + isbn + "\nDisponibilidad: " + disponibilidad;
+    }
+
+    public void setDisponibilidad(boolean disponibilidad) {
+        this.disponibilidad = disponibilidad;
+    }
 }
