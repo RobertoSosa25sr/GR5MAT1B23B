441530b1391988bfe2d2278e368989a4dca55499
jairo <jairo@example.com>
2023-11-29 20:18:01 -0500

    Actualizacion catalogo libros

--- src/main/java/modelo/CatalogoLibros.java
+++ src/main/java/modelo/CatalogoLibros.java
@@ -13,6 +13,7 @@ public class CatalogoLibros {
         listaLibros.add(new Libro("El Señor de los Anillos", "J.R.R. Tolkien", "Fantasía", "978-84-450-7425-0", true));
         listaLibros.add(new Libro("Cien años de soledad", "Gabriel García Márquez", "Realismo mágico", "978-84-376-0494-7", true));
         listaLibros.add(new Libro("1984", "George Orwell", "Ciencia ficción", "978-84-376-0494-8", true));
+        listaLibros.add(new Libro("To Kill a Mockingbird", "Harper Lee", "Ficción clásica", "978-84-376-0497-7", true));
 
     }
 
