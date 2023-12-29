86b9d66c77e7d4150f997fa74500e023762c5553
RobertoSosa25sr <ricardo_sr25@outlook.com>
2023-12-28 20:42:11 -0500

    prueba

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,5 +1,6 @@
-import static org.junit.Assert.fail;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.fail;
+
 public class MainTest {
 
     @Test
