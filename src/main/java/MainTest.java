ea1fca0c7b27c86435a4cde296a235689291b80f
RobertoSosa25sr <ricardo_sr25@outlook.com>
2023-12-28 20:11:40 -0500

    prueba

---
+++ src/main/java/MainTest.java
@@ -0,0 +1,10 @@
+import org.testng.annotations.Test;
+import static junit.framework.Assert.assertTrue;
+
+public class MainTest {
+
+    @Test
+    public void testMainMethod() {
+        assertTrue(true); // Un ejemplo simple de afirmación
+    }
+}
